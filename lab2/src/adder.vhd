96d49d7b4172ca2708a938f118968731614bea62
Velkaobet <72571270+Velkaobet@users.noreply.github.com>
2020-11-26 18:47:36 +0100

    change
    correction

--- lab2/src/adder.vhd
+++ lab2/src/adder.vhd
@@ -492,7 +492,7 @@ begin
 
 	-- SUM OUT ----------------------------------------
 
-	Output <= std_logic_vector(unsigend(operand1) + unsigned(operand2));
+	Output <= std_logic_vector(unsignend(operand1) + unsigned(operand2));
 
 
 end architecture RTL;
