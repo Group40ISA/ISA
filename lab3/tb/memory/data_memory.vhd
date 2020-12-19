0e8ce9b0ae610f83138b1b865a058b1cd5a2ba55
Michelangelo <s275289@studenti.polito.it>
2020-12-19 10:13:30 +0100

    apdate data mem
    insert init in data mem to reset it

--- lab3/tb/memory/data_memory.vhd
+++ lab3/tb/memory/data_memory.vhd
@@ -12,8 +12,9 @@ ENTITY data_memory IS
     GENERIC (
         address_parallelism : INTEGER := 32;
         data_parallelism : INTEGER := 32;
-        memory_depth : integer := 5);
+        memory_depth : INTEGER := 5);
     PORT (
+        init : IN STD_LOGIC;
         input_data : IN STD_LOGIC_VECTOR(data_parallelism - 1 DOWNTO 0);
         address : IN STD_LOGIC_VECTOR(address_parallelism - 1 DOWNTO 0);
         end_code, read_en, write_en : IN STD_LOGIC;
@@ -24,18 +25,20 @@ END ENTITY data_memory;
 ARCHITECTURE rtl OF data_memory IS
 
     FILE text_content : text;
-    
-    TYPE memory IS ARRAY ( (2 ** memory_depth) - 1 DOWNTO 0) OF STD_LOGIC_VECTOR(data_parallelism - 1 DOWNTO 0);
+
+    TYPE memory IS ARRAY ((2 ** memory_depth) - 1 DOWNTO 0) OF STD_LOGIC_VECTOR(data_parallelism - 1 DOWNTO 0);
     SIGNAL data_mem : memory;
 
 BEGIN
 
-    proc_out : PROCESS (end_code)
+    proc_out : PROCESS (end_code, init)
         VARIABLE out_data_line : line;
         VARIABLE out_pointer : INTEGER := 0; -- variable that points at successive lines of the memory for every written line
     BEGIN
         file_open(text_content, "C:\Users\39373\Documents\Uni\Magistrale\IntegratedSystemArchitecture\Labs\ISA\lab3\tb\memory\content_data.txt", write_mode);
-        IF end_code = '1' THEN
+        IF init = '1' THEN
+            data_mem <= (OTHERS => (OTHERS => '0'));
+        ELSIF end_code = '1' THEN
             WHILE (out_pointer < 2 ** memory_depth) LOOP
                 write(out_data_line, to_integer(signed(data_mem(out_pointer))));
                 writeline(text_content, out_data_line);
@@ -44,25 +47,25 @@ BEGIN
         END IF;
         file_close(text_content);
     END PROCESS proc_out;
-    
+
     --------------------------------------------------------------------------------
     --  process to have a transparent read/write data_memory                      --
     --------------------------------------------------------------------------------
 
-    write_proc: process(write_en, address) 
-    begin
-        if write_en = '1' then
+    write_proc : PROCESS (write_en, address)
+    BEGIN
+        IF write_en = '1' THEN
             data_mem(to_integer(unsigned(address))) <= input_data;
-        end if;
-    end process write_proc;
-        
-    read_proc: process(read_en, address)
-    begin
-        if read_en = '1' then
+        END IF;
+    END PROCESS write_proc;
+
+    read_proc : PROCESS (read_en, address)
+    BEGIN
+        IF read_en = '1' THEN
             output_data <= data_mem(to_integer(unsigned(address)));
-        else 
-            output_data <= (others => '0');
-        end if;
-    end process read_proc;
-    
+        ELSE
+            output_data <= (OTHERS => '0');
+        END IF;
+    END PROCESS read_proc;
+
 END ARCHITECTURE rtl;
